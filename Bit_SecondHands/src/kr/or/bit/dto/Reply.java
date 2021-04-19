8e6f78e365620625964a7e014e9cbcb9a7dfcda7
STAY-TaeHui <masta95@naver.com>
2021-04-19 17:44:43 +0900

    server setup

--- Bit_SecondHands/src/kr/or/bit/dto/Reply.java
+++ Bit_SecondHands/src/kr/or/bit/dto/Reply.java
@@ -1,91 +1,69 @@
 package kr.or.bit.dto;
+
 import java.util.Date;
 
-
-public class Reply {
-	private int no;
-	private String writer;
-	private String userid;
-	private String pwd;
-	private String content;
-	private Date writedate;
-	private int idx_fk;
+public class reply {
+	private int rp_num;
+	private int p_num;
+	private String storename;
+	private String rp_content;
+	private Date rp_date;
 	
-	public Reply() {}
+	public reply() {}
 
-	public Reply(int no, String writer, String userid, String pwd, String content, Date writedate, int idx_fk) {
+	public reply(int rp_num, int p_num, String storename, String rp_content, Date rp_date) {
 		super();
-		this.no = no;
-		this.writer = writer;
-		this.userid = userid;
-		this.pwd = pwd;
-		this.content = content;
-		this.writedate = writedate;
-		this.idx_fk = idx_fk;
+		this.rp_num = rp_num;
+		this.p_num = p_num;
+		this.storename = storename;
+		this.rp_content = rp_content;
+		this.rp_date = rp_date;
 	}
 
-	public int getNo() {
-		return no;
+	public int getRp_num() {
+		return rp_num;
 	}
 
-	public void setNo(int no) {
-		this.no = no;
+	public void setRp_num(int rp_num) {
+		this.rp_num = rp_num;
 	}
 
-	public String getWriter() {
-		return writer;
+	public int getP_num() {
+		return p_num;
 	}
 
-	public void setWriter(String writer) {
-		this.writer = writer;
+	public void setP_num(int p_num) {
+		this.p_num = p_num;
 	}
 
-	public String getUserid() {
-		return userid;
+	public String getStorename() {
+		return storename;
 	}
 
-	public void setUserid(String userid) {
-		this.userid = userid;
+	public void setStorename(String storename) {
+		this.storename = storename;
 	}
 
-	public String getPwd() {
-		return pwd;
+	public String getRp_content() {
+		return rp_content;
 	}
 
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
+	public void setRp_content(String rp_content) {
+		this.rp_content = rp_content;
 	}
 
-	public String getContent() {
-		return content;
+	public Date getRp_date() {
+		return rp_date;
 	}
 
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public Date getWritedate() {
-		return writedate;
-	}
-
-	public void setWritedate(Date writedate) {
-		this.writedate = writedate;
-	}
-
-	public int getIdx_fk() {
-		return idx_fk;
-	}
-
-	public void setIdx_fk(int idx_fk) {
-		this.idx_fk = idx_fk;
+	public void setRp_date(Date rp_date) {
+		this.rp_date = rp_date;
 	}
 
 	@Override
 	public String toString() {
-		return "reply [no=" + no + ", writer=" + writer + ", userid=" + userid + ", pwd=" + pwd + ", content=" + content
-				+ ", writedate=" + writedate + ", idx_fk=" + idx_fk + "]";
-	}
-	
-	
+		return "reply [rp_num=" + rp_num + ", p_num=" + p_num + ", storename=" + storename + ", rp_content="
+				+ rp_content + ", rp_date=" + rp_date + "]";
+	};
 	
 }
